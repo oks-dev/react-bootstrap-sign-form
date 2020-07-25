b865976c5009b3846cabe7727dc2c5853f303510
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-25 15:35:09 +0300

    added data to form, validation for password

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const list = [
   { id: 1, strong: 'Build better habits ', text: 'by surrounding yourself with productive makers.', icon: 'ship' },
@@ -7,6 +7,29 @@ const list = [
 ]
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false)
+  const [data, setData] = useState({
+    fullName: '',
+    userName: '',
+    email: '',
+    password: ''
+  })
+  const [passwordError, setPasswordError] = useState('')
+
+  const handleCheck = e => {
+    setIsChecked(e.target.checked)
+  }
+  const handleInputChange = e => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value })
+    name === 'password' && value.length < 6 ? setPasswordError('Minimum 6 characters') : setPasswordError('')
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setData({ ...data, fullName: '', userName: '', email: '', password: '' })
+    setIsChecked(false)
+  }
+
   return (
     <div className='container-md wrapper'>
       <div className='content shadow'>
@@ -18,8 +41,8 @@ function App() {
               {list.map(item => (
                 <li className='d-flex mb-2 pb-2' key={item.id}>
                   <span className='icon'><img src={`${item.icon}.png`} alt='' /></span>
-                  <div className="pl-2">
-                    <p className="font-size-md mb-0">
+                  <div className='pl-2'>
+                    <p className='font-size-md mb-0'>
                       <strong>{item.strong}</strong>
                       {item.text}
                     </p>
@@ -31,66 +54,103 @@ function App() {
           </div>
           <div className='col-12 col-md-6 col-lg-8 p-4'>
 
-            <form className="form-sign py-5 p-lg-5">
+            <form className='form-sign py-5 p-lg-5' onSubmit={handleSubmit}>
               <div className='form-toggle text-muted small'>Already a member? <a href='#'>Sign in</a></div>
-              <div className="mb-4">
-                <h1 className="h2 mb-3 font-weight-normal">Sign up</h1>
+              <div className='mb-4'>
+                <h1 className='h2 mb-3 font-weight-normal'>Sign up</h1>
               </div>
 
-              <div className="form-row text-center">
-                <div className="col-8">
-                  <a className="btn btn-lg btn-primary btn-image-wrap" href="#" role="button">
+              <div className='form-row text-center'>
+                <div className='col-8'>
+                  <a className='btn btn-lg btn-primary btn-image-wrap' href='#' role='button'>
                     <img src='google.svg' className='btn-image' alt='Sign with Google' />
                     Sign up with Google
                   </a>
-                  {/* <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up with Google</button> */}
                 </div>
-                <div className="col d-flex justify-content-end">
-                  <a className="btn btn-lg btn-light btn-social" href="#" role="button">
+                <div className='col d-flex justify-content-end'>
+                  <a className='btn btn-lg btn-light btn-social' href='#' role='button'>
                     <img src='twitter.svg' alt='Sign with Twitter' />
                   </a>
                 </div>
-                <div className="col d-flex justify-content-end">
-                  <a className="btn btn-lg btn-light btn-social" href="#" role="button">
+                <div className='col d-flex justify-content-end'>
+                  <a className='btn btn-lg btn-light btn-social' href='#' role='button'>
                     <img src='fb.svg' alt='Sign with Facebook' />
                   </a>
                 </div>
               </div>
-              <div className=" divider my-4 text-muted">Or</div>
+              <div className=' divider my-4 text-muted'>Or</div>
 
-              <div className="form-row">
-                <div className="col">
-                  <label htmlFor="fullName">Full name</label>
-                  <input type="text" id="fullName" className="form-control bg-light border-0" placeholder="" autoFocus />
+              <div className='form-row'>
+                <div className='col'>
+                  <label htmlFor='fullName'>Full name</label>
+                  <input
+                    type='text'
+                    id='fullName'
+                    name='fullName'
+                    className='form-control bg-light border-0'
+                    required
+                    value={data.fullName}
+                    onChange={handleInputChange}
+                    autoFocus
+                  />
                 </div>
-                <div className="col">
-                  <label htmlFor="userName">Username</label>
-                  <input type="text" id="userName" className="form-control bg-light border-0" placeholder="" />
+                <div className='col'>
+                  <label htmlFor='userName'>Username</label>
+                  <input
+                    type='text'
+                    id='userName'
+                    name='userName'
+                    className='form-control bg-light border-0'
+                    required
+                    value={data.userName}
+                    onChange={handleInputChange}
+                  />
                 </div>
               </div>
 
 
-              <div className="form-label-group mt-3">
-                <label htmlFor="inputEmail">Email Address</label>
-                <input type="email" id="inputEmail" className="form-control bg-light border-0" placeholder="" required="" />
+              <div className='form-label-group mt-3'>
+                <label htmlFor='inputEmail'>Email Address</label>
+                <input
+                  type='email'
+                  id='inputEmail'
+                  name='email'
+                  className='form-control bg-light border-0'
+                  required
+                  value={data.email}
+                  onChange={handleInputChange}
+                />
               </div>
 
-              <div className="form-label-group mt-3">
-                <label htmlFor="inputPassword">Password</label>
-                <input type="password" id="inputPassword" className="form-control bg-light border-0" placeholder="6+ characters" required="" />
+              <div className='form-label-group mt-3'>
+                <label htmlFor='inputPassword'>Password</label>
+                <input
+                  type='password'
+                  id='inputPassword'
+                  name='password'
+                  className='form-control bg-light border-0'
+                  placeholder='6+ characters'
+                  required
+                  value={data.password}
+                  onChange={handleInputChange}
+                />
+                {passwordError.length > 0 && <div className='small text-danger'>{passwordError}</div>}
               </div>
 
-              <div className="form-group mt-3">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" id="gridCheck" />
-                  <label className="form-check-label text-muted" htmlFor="gridCheck">
+              <div className='form-group mt-3'>
+                <div className='form-check'>
+                  <input className='form-check-input' type='checkbox' id='gridCheck' checked={isChecked} onChange={(e) => handleCheck(e)} />
+                  <label className='form-check-label text-muted' htmlFor='gridCheck'>
                     Creating an account means you're okay with our <a href='#'>Terms of Service</a>, <a href='#'>Privacy Policy</a>, and our default <a href='#'>Notification Settings</a>.
                   </label>
                 </div>
               </div>
 
               <div className='col-12 col-md-6 pl-0'>
-                <button className="btn btn-danger btn-block" type="submit">Create account</button>
+                <button className='btn btn-danger btn-block'
+                  disabled={!isChecked || passwordError}
+                  type='submit'
+                >Create account</button>
               </div>
             </form>
 
