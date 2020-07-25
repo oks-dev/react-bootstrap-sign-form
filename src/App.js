30e51a72f086fee7deca63965c10dd850e8a115f
Oksana Tkachenko <coatceatl@gmail.com>
2020-07-25 08:17:25 +0300

    added left part of login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const list = [
+  { id: 1, strong: 'Build better habits ', text: 'by surrounding yourself with productive makers.', icon: 'ship' },
+  { id: 2, strong: 'Get advice & feedback ', text: 'from experienced bootstrappers.', icon: 'chat' },
+  { id: 3, strong: 'Stay accountable ', text: 'by publicly sharing your progress.', icon: 'checked' },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-sm wrapper'>
+      <div className='content p-4 shadow'>
+        <div className='row'>
+          <div className='col-5'>
+            <h1 className='title h3'>Join a community of makers shipping together.</h1>
+            <div className='subtitle my-3'>From first-ti,e entrepreneurs to solo-founders bootstrapping $300,000/year companies.</div>
+            <ul className='list m-2'>
+              {list.map(item => (
+                <li className='d-flex mb-2 pb-2' key={item.id}>
+                  <span className='icon'><img src={`${item.icon}.png`} alt='' /></span>
+                  <div className="pl-2">
+                    <p className="font-size-md mb-0">
+                      <strong>{item.strong}</strong>
+                      {item.text}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='col-8'></div>
+        </div>
+      </div>
     </div>
   );
 }
